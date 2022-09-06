c9e77593dd7d417de424e31c83a391fbb4044744
reegee15 <rcadeau15@gmail.com>
2022-09-06 15:02:36 -0400

    wrote code to display player and computer score

--- game.js
+++ game.js
@@ -1,28 +1,30 @@
 const score = document.querySelector("#score");
 let playerSelection, computerSelection;
-let btn1 , btn2, btn3, reset;
+let btn1 , btn2, btn3, reset, score_text;
 let pWins = 0, cWins = 0;
 buttons = document.querySelector("#buttons");
 btn1 = document.querySelector(".btn1");
 btn2 = document.querySelector(".btn2");
 btn3 = document.querySelector(".btn3");
 reset = document.querySelector(".reset-btn");
+score_text = document.querySelector("#score-text");
 
 
 let pDisplay=document.createElement("div");
 pDisplay.className = "display";
 score.appendChild(pDisplay);
-pDisplay.textContent = 0;
+pDisplay.textContent = "Player Score: " + 0;
 let cDisplay=document.createElement("div");
 score.appendChild(cDisplay);
-cDisplay.textContent = 0;
+cDisplay.textContent = "Computer Score: " + 0;
 
 reset.addEventListener("click", function(){
     console.clear();
     pWins = 0;
     cWins = 0;
-    pDisplay.textContent= 0;
-    cDisplay.textContent = 0;
+    pDisplay.textContent= "Player Score: " + 0;
+    cDisplay.textContent = "Computer Score: " + 0;
+    score_text.textContent = "";
     btn1.disabled = false;
     btn2.disabled = false;
     btn3.disabled = false;
@@ -58,23 +60,23 @@ function computerChoice(){
 function playRound(playerSelection, computerSelection){
     computerSelection = computerChoice();
     if(playerSelection === computerSelection){
-        console.log(`Tie. ${playerSelection} = ${computerSelection}`);
+        score_text.textContent = `Tie. ${playerSelection} = ${computerSelection}`;
         return "TIE";
     }
     else if(playerSelection === "Rock" && computerSelection === "Scissors"){
-        console.log(`You Win. ${playerSelection} beats ${computerSelection}`);
+        score_text.textContent = `You Win. ${playerSelection} beats ${computerSelection}`;
         return "WIN";
     }
     else if(playerSelection === "Paper" && computerSelection === "Rock"){
-        console.log(`You Win. ${playerSelection} beats ${computerSelection}`);
+        score_text.textContent = `You Win. ${playerSelection} beats ${computerSelection}`;
         return "WIN";
     }
     else if(playerSelection === "Scissors" && computerSelection === "Paper"){
-        console.log(`You Win. ${playerSelection} beats ${computerSelection}`);
+        score_text.textContent = `You Win. ${playerSelection} beats ${computerSelection}`;
         return "WIN";
     }
     else{
-        console.log(`You Lose. ${computerSelection} beats ${playerSelection}`);
+        score_text.textContent = `You Lose. ${computerSelection} beats ${playerSelection}`;
         return "LOSE";
     }
 }
@@ -83,11 +85,11 @@ function game(){
     let round = playRound(playerSelection, computerSelection)
     if(round === "WIN"){
         pWins++;
-        pDisplay.textContent = pWins;
+        pDisplay.textContent = "Player Score: " + pWins;
         console.log(pWins);
 
         if(pWins === 5){
-            console.log("Player Wins!");
+            score_text.textContent = "Player Wins!";
             btn1.disabled = true;
 			btn2.disabled = true;
 			btn3.disabled = true;
@@ -95,11 +97,11 @@ function game(){
     }
     else if(round === "LOSE"){
         cWins++;
-        cDisplay.textContent = cWins;
+        cDisplay.textContent = "Computer Score: " + cWins;
         console.log(cWins);
 
         if(cWins === 5){
-            console.log("Computer Wins!");
+            score_text.textContent = "Computer Wins!";
             btn1.disabled = true;
 			btn2.disabled = true;
 			btn3.disabled = true;
@@ -107,5 +109,3 @@ function game(){
     }
     
 }
-
-
